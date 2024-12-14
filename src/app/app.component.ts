f8c9766de9bbc867bc772563b0058658dbe4a9f4
hamzathul dilshad <diluhd99@gmail.com>
2024-12-14 13:45:40 +0530

    Todo fully completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+
+export interface TodoItem{
+  id:number;
+  task:string;
+  completed:boolean;
+}
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+ todoList:TodoItem[]=[];
+ newTask:string=''
+
+ addTask():void{
+  if(this.newTask.trim() !== ''){
+    const newTodoItem:TodoItem={
+      id:Date.now(),
+      task:this.newTask,
+      completed:false
+    }
+
+    this.todoList.push(newTodoItem)
+    this.newTask=''
+    console.log(this.todoList);
+    
+  }
+ }
+
+ toggleComplete(index:number):void{
+  console.log(index)
+  this.todoList[index].completed=!this.todoList[index].completed
+ }
+
+ deleteTask(id:number):void{
+  this.todoList=this.todoList.filter((item)=>item.id!=id)
+  console.log(this.todoList);
+  
+ }
+
+
+
+}
